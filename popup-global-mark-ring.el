ddb9456f1cc274d31a80dfcf974f19d52205fb4a
whitypig <whitypig@gmail.com>
2010-04-27 07:11:42 +0900

    Made some progress.

--- popup-global-mark-ring.el
+++ popup-global-mark-ring.el
@@ -1,5 +1,44 @@
 (require 'popup)
 
+(defvar popup-global-mark-ring-menu-width 70
+  "Width of popup menu")
+
+(defun popup-global-mark-ring ()
+  "Show global mark ring menu and go to the place selected."
+  (interactive)
+  (let ((item nil)
+        (num 0))
+    ;; Show menu and get selection
+    (setq item (popup-menu* (popup-global-mark-ring-menu)
+                            :scroll-bar t
+                            :margin t
+                            :width popup-global-mark-ring-menu-width))
+    (when item
+      (when (string-match "^\\([0-9]+\\):.*" item)
+        (setq num (1- (string-to-number (match-string 1 item))))
+        (setq marker (nth num global-mark-ring))
+        ;; Make current-location maker from the current location
+        ;; and push it into mark-ring if it is not
+        (setq current-marker (point-marker))
+        (unless (member current-marker global-mark-ring)
+          (push-mark))
+        ;; now, switch to the buffer and go to the pos specified by the marker
+        (switch-to-buffer (marker-buffer marker))
+        (goto-char (marker-position marker))))))
+
+;;  test code
+(ignore-errors
+  (popup-menu* (popup-global-mark-ring) :margin t :scroll-bar t))
+
+;; TODO
+(defun popup-global-mark-ring-current ())
+
+;; TODO
+(defun popup-global-mark-ring-next ())
+
+;; TODO
+(defun popup-global-mark-ring-prev ())
+
 (defun popup-global-mark-ring-menu ()
   "Return the list of lines with each line containing marker info.
 Iterating `global-mark-ring', make and return a list consisting of
@@ -25,12 +64,3 @@ marker information that can be acquired from each element in `global-mark-ring'"
                                (1+ i) linenum bufname
                                (buffer-substring-no-properties start end)) t))))
     ret))
-
-;;  test code
-(popup-menu* (popup-global-mark-ring-menu) :margin t :scroll-bar t)
-
-(defun popup-global-mark-ring-current ())
-
-(defun popup-global-mark-ring-next ())
-
-(defun popup-global-mark-ring-prev ())
