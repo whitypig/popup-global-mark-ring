e443ff355291a41fc7a8a1a3e874f58554a071f3
U-WPDESKTOP\whitypig <whitypig@wpdesktop.(none)>
2010-06-02 04:14:04 +0900

    Fix comment.

--- popup-global-mark-ring.el
+++ popup-global-mark-ring.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2010  whitypig
 
 ;; Author: whitypig <whitypig@gmail.com>
-;; Keywords: lisp popup
-;; Version: 0.1
+;; Keywords: lisp popup emacs meadow
+;; Version: 0.11
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
